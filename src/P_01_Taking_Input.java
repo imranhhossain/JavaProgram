c87b05d500cafa1b8105cbd129975305a5e196b4
imranhhossain <imran.hhossain99@gmail.com>
2026-06-17 11:56:18 +0600

    taking input from the user

---
+++ src/P_01_Taking_Input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class P_01_Taking_Input {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+//        System.out.println("Enter the number1 : ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter the number2 : ");
+//        int b = sc.nextInt();
+//
+//        int sum = a + b;
+//        System.out.println("The sum of  a and b is : " + sum);
+
+        String str = sc.nextLine();
+        System.out.println(str);
+
+
+    }
+}
