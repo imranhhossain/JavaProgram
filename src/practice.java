7265f23b08b6c0d10b7d65da0fd8b3e459ae2cb1
imranhhossain <imran.hhossain99@gmail.com>
2026-06-20 12:10:28 +0600

    practice with string in the practice file

--- src/practice.java
+++ src/practice.java
@@ -2,9 +2,21 @@ import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
+        //convert string to lowercase
+        String str = "Imran Hossain";
+        System.out.println(str.toLowerCase());
 
-        int a = 5;
-        System.out.println("The value of a is : "+ a++);
-        System.out.println("The value of a is : "+ a);
+        //convert string to Uppercase
+        System.out.println(str.toUpperCase());
+
+        //replace space with underscore
+        String text = "   Hello   world   ";
+        System.out.println(text.replace(' ', '_'));
+
+        //replace Word
+        String letter = "Hello <|name|>, thank a lot";
+        letter = letter.replace("<|name|>", "Imran");
+        System.out.println(letter);
     }
 }
+
