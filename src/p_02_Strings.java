ae046791b76f5e3941b7627e1bd6be0468b54ad2
imranhhossain <imran.hhossain99@gmail.com>
2026-06-20 11:09:11 +0600

    practice with String and add practice file

---
+++ src/p_02_Strings.java
@@ -0,0 +1,12 @@
+public class p_02_Strings {
+    public static void main(String[] args) {
+//        String name = new String("Imran Hossain");
+//        String name = "Imran Hossain";
+//        System.out.println(name);
+        int a = 5;
+        float b = 5.6f;
+        System.out.printf("the value of a is : %d and the value of b is : %f \n", a, b);
+        System.out.printf("the value of a is : %d \n the value of b is : %f \n", a, b);
+        System.out.printf("the value of a is : %d \n the value of b is : %.2f", a, b);
+    }
+}
---
+++ src/practice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("The value of a is : "+ a++);
+        System.out.println("The value of a is : "+ a);
+    }
+}
