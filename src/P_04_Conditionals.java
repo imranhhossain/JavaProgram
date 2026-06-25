8afb5a7bc1055a14b65e86fc5eefb7f12078c97e
imranhhossain <imran.hhossain99@gmail.com>
2026-06-26 01:34:06 +0600

    implement if else with user input

--- src/P_04_Conditionals.java
+++ src/P_04_Conditionals.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class P_04_Conditionals {
     public static void main(String[] args) {
-        int age = 18;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your Age");
+        int age = sc.nextInt();
+
         if(age>=18){
             System.out.println("You can drive...");
         }
