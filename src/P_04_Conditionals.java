52da381c977e3b9b621861f8352a706a38d71590
imranhhossain <imran.hhossain99@gmail.com>
2026-06-26 01:30:24 +0600

    practice with if else condition

---
+++ src/P_04_Conditionals.java
@@ -0,0 +1,11 @@
+public class P_04_Conditionals {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age>=18){
+            System.out.println("You can drive...");
+        }
+        else{
+            System.out.println("You cannot drive...");
+        }
+    }
+}
