38e5cdd7a9980a217ca569d5b5c7181e41153ef7
imranhhossain <imran.hhossain99@gmail.com>
2026-06-26 02:42:14 +0600

    implement switch statement

--- src/p_05_elseif_Switch_Case_Statement.java
+++ src/p_05_elseif_Switch_Case_Statement.java
@@ -6,19 +6,32 @@ public class p_05_elseif_Switch_Case_Statement {
         System.out.println("Enter Your Age");
         int age = sc.nextInt();
 
-        //else if Statement
-        if(age > 56){
-            System.out.println("You are Experienced");
-        }
-        else if (age > 45) {
-            System.out.println("You are semi Experienced");
-        }
-        else if (age > 36){
-            System.out.println("You Are less Experienced");
-        }
-        else {
-            System.out.println("You are Not Experienced");
-        }
+//        //else if Statement
+//        if(age > 56){
+//            System.out.println("You are Experienced");
+//        }
+//        else if (age > 45) {
+//            System.out.println("You are semi Experienced");
+//        }
+//        else if (age > 36){
+//            System.out.println("You Are less Experienced");
+//        }
+//        else {
+//            System.out.println("You are Not Experienced");
+//        }
 
+        // Switch Statement
+        switch (age){
+            case 18:
+                System.out.println("You are become an adult");
+                break;
+            case 24:
+                System.out.println("You are going to join a job");
+                break;
+            case 65:
+                System.out.println("You are going to get retired!");
+            default:
+                System.out.println("Enjoy your life");
+        }
     }
 }
