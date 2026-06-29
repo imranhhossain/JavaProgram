ee7818aaf6255477cd4b509c658bb94efb13fb06
imranhhossain <imran.hhossain99@gmail.com>
2026-06-29 11:02:07 +0600

    practice with loops

---
+++ src/p_06_Loops.java
@@ -0,0 +1,31 @@
+public class p_06_Loops {
+    public static void main(String[] args) {
+        int i = 20;
+        //while loops
+//        while(i<=200){
+//            System.out.println(i);
+//            i++;
+//        }
+
+        //do-while loops
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i <=10);
+
+        //for loops
+//        for(int j=0; j<=11; j++){
+//            System.out.println(j);
+//        }
+
+        //decrementing loops
+//        for(int k=5; k>0; k--){
+//            System.out.println(k);
+//        }
+
+        for(int k=10; k!=0; k--){
+            System.out.println(k);
+        }
+
+    }
+}
