ea7e156890959b854a84df682312d24f1788a1ed
imranhhossain <imran.hhossain99@gmail.com>
2026-08-12 09:55:08 +0600

    practice on access modifier and constructor

--- src/P_00_Practice.java
+++ src/P_00_Practice.java
@@ -1,46 +1,86 @@
+class Cylinder{
+    private int height;
+    private int radius;
 
-class Employee{
-    int salary;
-    String name;
 
-    public int getSalary(){
-        return salary;
+    public Cylinder(int height, int radius){
+        this.height = height;
+        this.radius = radius;
     }
 
-    public String getName(){
-        return name;
+
+//    public int getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(int height) {
+//        this.height = height;
+//    }
+//
+//    public int getRadius() {
+//        return radius;
+//    }
+//
+//    public void setRadius(int radius) {
+//        this.radius = radius;
+//    }
+
+    public double surfaceArea(){
+        return 2*Math.PI*radius*height + 2 *Math.PI*radius*radius;
     }
-    public void setName(String n){
-        name = n;
+    public double Volume(){
+        return Math.PI*radius*radius*height;
     }
+
 }
 
-class Square{
-    int side;
-
-    public int area(){
-        return side*side;
+class Rectangle{
+    private int length;
+    private int breadth;
+    public Rectangle(){
+        this.length = 4;
+        this.breadth = 5;
     }
-    public int perameter(){
-        return 4*side;
+
+
+    public Rectangle(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
     }
 }
 
 public class P_00_Practice {
     public static void main(String[] args) {
 
-        //practice 1
-        Employee emp = new Employee();
-        emp.salary = 1300;
+        //practice 1- create getter and setter for set value
+//        Cylinder myCylinder = new Cylinder();
+//        myCylinder.setHeight(12);
+//        myCylinder.setRadius(9);
+//
+//        System.out.println(myCylinder.getHeight());
+//        System.out.println(myCylinder.getRadius());
 
-        emp.setName("jhon");
-        System.out.println("The Employee Salary is : " + emp.getSalary());
-        System.out.println("The Employee name is : " + emp.getName());
+        //practice 2
+//        System.out.println(myCylinder.surfaceArea());
+//        System.out.println(myCylinder.Volume());
 
-        Square sq = new Square();
-        sq.side = 3;
-        System.out.println( sq.area());
-        System.out.println(sq.perameter());
+        //practice 3 - using constructor
+        Cylinder myCylinder = new Cylinder(12, 9);
+        System.out.println(myCylinder.surfaceArea());
+        System.out.println(myCylinder.Volume());
+
+        //practice 4
+        Rectangle myRectangle = new Rectangle(12, 9);
+        System.out.println(myRectangle.getLength());
+        System.out.println(myRectangle.getBreadth());
     }
 }
 
