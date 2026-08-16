d721f28e5b8c3f9c81c77e455319d875de727410
imranhhossain <imran.hhossain99@gmail.com>
2026-08-16 08:37:51 +0600

    Practice on abstract and interface in practice file

--- src/P_00_Practice.java
+++ src/P_00_Practice.java
@@ -1,86 +1,52 @@
-class Cylinder{
-    private int height;
-    private int radius;
-
-
-    public Cylinder(int height, int radius){
-        this.height = height;
-        this.radius = radius;
-    }
-
-
-//    public int getHeight() {
-//        return height;
-//    }
-//
-//    public void setHeight(int height) {
-//        this.height = height;
-//    }
-//
-//    public int getRadius() {
-//        return radius;
-//    }
-//
-//    public void setRadius(int radius) {
-//        this.radius = radius;
-//    }
-
-    public double surfaceArea(){
-        return 2*Math.PI*radius*height + 2 *Math.PI*radius*radius;
-    }
-    public double Volume(){
-        return Math.PI*radius*radius*height;
-    }
-
+abstract class Pen{
+    abstract void write();
+    abstract void refill();
 }
 
-class Rectangle{
-    private int length;
-    private int breadth;
-    public Rectangle(){
-        this.length = 4;
-        this.breadth = 5;
+class FountainPen extends Pen{
+    @Override
+    void write() {
+        System.out.println("write");
     }
 
-
-    public Rectangle(int length, int breadth){
-        this.length = length;
-        this.breadth = breadth;
+    @Override
+    void refill() {
+        System.out.println("refill");
     }
-
-    public int getLength() {
-        return length;
+    void changeNib(){
+        System.out.println("Changing the nib");
     }
+}
 
-    public int getBreadth() {
-        return breadth;
+abstract class Telephone{
+    abstract void ring();
+    abstract void lift();
+    abstract void disconnect();
+}
+
+class SmartPhone2 extends Telephone{
+    @Override
+    void ring() {
+        System.out.println("ringing...");
+    }
+    @Override
+    void lift() {
+        System.out.println("lift");
+    }
+    @Override
+    void disconnect() {
+        System.out.println("disconnect....");
     }
 }
 
 public class P_00_Practice {
     public static void main(String[] args) {
 
-        //practice 1- create getter and setter for set value
-//        Cylinder myCylinder = new Cylinder();
-//        myCylinder.setHeight(12);
-//        myCylinder.setRadius(9);
-//
-//        System.out.println(myCylinder.getHeight());
-//        System.out.println(myCylinder.getRadius());
-
-        //practice 2
-//        System.out.println(myCylinder.surfaceArea());
-//        System.out.println(myCylinder.Volume());
-
-        //practice 3 - using constructor
-        Cylinder myCylinder = new Cylinder(12, 9);
-        System.out.println(myCylinder.surfaceArea());
-        System.out.println(myCylinder.Volume());
-
-        //practice 4
-        Rectangle myRectangle = new Rectangle(12, 9);
-        System.out.println(myRectangle.getLength());
-        System.out.println(myRectangle.getBreadth());
+        //Demonstrate Polymorphism
+        Telephone telephone = new SmartPhone2();
+        telephone.ring();
+        telephone.lift();
+        telephone.disconnect();
     }
 }
 
